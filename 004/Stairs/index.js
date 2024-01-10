9b422d62b87d711c0d6c7641242fee08742cdfe7
eyedave <aydaaheidari@gmail.com>
2024-01-10 23:15:32 +0330

    fully

--- 004/Stairs/index.js
+++ 004/Stairs/index.js
@@ -3,8 +3,5 @@ let text=document.querySelectorAll('.text');
 document.addEventListener('mousemove',(e)=>{
   let y=e.pageY - (window.innerHeight/2);
 
-  //text.style.transform = `translateY(${y}px)`;
-  
   text.forEach(function (element) {element.style.transform = `translateY(${y}px)`;})
-
 })

d8f3b33b0ba4aa713dedcc09dda34b184e4094b9
Aida <100238066+eyedave@users.noreply.github.com>
2024-03-07 15:53:43 +0330

    hangman game

--- 015/script.js
+++ 015/script.js
@@ -24,7 +24,7 @@ const rightSound = document.getElementById('right');
 const wrongSound = document.getElementById('wrong');
 
 let inputs = [];
-let keys = []; // inam baaiid midoonam be daardet bokhore
+let keys = []; 
 
 let currentIndex = 0; 
 let mistake = 0; 

71f5a3b04429a1d8a3a221c3c81060a1b29295b0
eyeedev <eyeedev@gamil.com>
2025-02-01 01:40:37 +0330

    added button-fixed grid lines

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let canvasPart = document.getElementById('canvas');
 let svg = document.getElementById('svg');
 let cssPlaying = document.getElementById('css');
 let buttons = document.getElementById('buttons');
+let git = document.querySelector('.git-simulator');
 
 function showCards(type) {
    for(let i = 0; i < cards.length; i++){
@@ -20,16 +21,54 @@ function showCards(type) {
     });
 }
 
-canvasPart.addEventListener('click', () => showCards('canvas'));
-games.addEventListener('click', ()=>showCards('game'));
-svg.addEventListener('click', ()=>showCards('svg'));
-cssPlaying.addEventListener('click', ()=>showCards('css'));
-buttons.addEventListener('click', ()=>showCards('buttons'));
+// canvasPart.addEventListener('click', () => showCards('canvas'));
+// games.addEventListener('click', ()=>showCards('game'));
+// svg.addEventListener('click', ()=>showCards('svg'));
+// cssPlaying.addEventListener('click', ()=>showCards('css'));
+// buttons.addEventListener('click', ()=>showCards('buttons'));
 
 
-all.addEventListener('click', ()=>{
-    for(let i = 0; i < cards.length; i++){
-        let card = cards[i]
-        card.style.display = '';
-       }
-});
+// all.addEventListener('click', ()=>{
+//     for(let i = 0; i < cards.length; i++){
+//         let card = cards[i]
+//         card.style.display = '';
+//        }
+// });
+
+if(!git) {
+    console.log("not found git");
+} else {
+    console.log(git);
+}
+
+for(let i = 0; i < 100; i++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    git.appendChild(square);
+}
+
+function getRandomColor(){
+    const shades = [
+        "#206d32",
+        "#006c36",
+        "#00432a",
+        "#00a649"
+    ];
+    return shades[Math.floor(Math.random() * shades.length)];
+}
+
+function animateSquares() {
+    const squares = document.querySelectorAll('.square');
+
+    setInterval(() =>{
+        const randomIndex = Math.floor(Math.random() * squares.length);
+        const randomSquare = squares[randomIndex];
+        randomSquare.style.backgroundColor = getRandomColor();
+
+        setTimeout(() => {
+            randomSquare.style.backgroundColor = '#151B22';
+        }, Math.random() * 2000 + 500);
+    }, 100)
+}
+
+animateSquares();

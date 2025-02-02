1a3d9766ec1afed20dce6566fdc46a9e1f81a0b2
eyeedev <eyeedev@gamil.com>
2025-02-02 23:43:02 +0330

    some new styles and logic

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let cards = document.querySelectorAll('.card');
 let all = document.getElementById('all');
 let games = document.getElementById('games');
 let canvasPart = document.getElementById('canvas');
@@ -6,34 +5,12 @@ let svg = document.getElementById('svg');
 let cssPlaying = document.getElementById('css');
 let buttons = document.getElementById('buttons');
 let git = document.querySelector('.git-simulator');
+let currentIndex = 0;
+const cards = document.querySelector('.card');
+const cardContainer = document.querySelector('.cards');
+const indicators = document.querySelectorAll('.svg-item');
+const cardWidth = cards[0].offsetWidth;
 
-function showCards(type) {
-   for(let i = 0; i < cards.length; i++){
-    let card = cards[i]
-    card.style.display = '';
-   }
-
-    cards.forEach(card => {
-        let cardType = card.getAttribute("data-type");
-        if (cardType !== type) {
-            card.style.display = "none";
-        }
-    });
-}
-
-// canvasPart.addEventListener('click', () => showCards('canvas'));
-// games.addEventListener('click', ()=>showCards('game'));
-// svg.addEventListener('click', ()=>showCards('svg'));
-// cssPlaying.addEventListener('click', ()=>showCards('css'));
-// buttons.addEventListener('click', ()=>showCards('buttons'));
-
-
-// all.addEventListener('click', ()=>{
-//     for(let i = 0; i < cards.length; i++){
-//         let card = cards[i]
-//         card.style.display = '';
-//        }
-// });
 
 if(!git) {
     console.log("not found git");
@@ -72,3 +49,49 @@ function animateSquares() {
 }
 
 animateSquares();
+
+
+document.addEventListener('DOMContentLoaded', () =>
+{
+    function updateSlider(){
+        cardContainer.style.transform =  `translateX(-${currentIndex * cardWidth}px)`;
+
+        indicators.forEach((indicator, index) => {
+            indicator.classList.toggle('active', index === currentIndex);
+        })
+    }
+
+    
+});
+
+
+
+// function showCards(type) {
+//    for(let i = 0; i < cards.length; i++){
+//     let card = cards[i]
+//     card.style.display = '';
+//    }
+//
+//     cards.forEach(card => {
+//         let cardType = card.getAttribute("data-type");
+//         if (cardType !== type) {
+//             card.style.display = "none";
+//         }
+//     });
+// }
+
+// canvasPart.addEventListener('click', () => showCards('canvas'));
+// games.addEventListener('click', ()=>showCards('game'));
+// svg.addEventListener('click', ()=>showCards('svg'));
+// cssPlaying.addEventListener('click', ()=>showCards('css'));
+// buttons.addEventListener('click', ()=>showCards('buttons'));
+
+
+// all.addEventListener('click', ()=>{
+//     for(let i = 0; i < cards.length; i++){
+//         let card = cards[i]
+//         card.style.display = '';
+//        }
+// });
+
+

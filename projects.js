5a2c843b1d8fd74a83b7d01e5983d231261f8105
eyeedev <eyeedev@gamil.com>
2025-02-09 17:13:57 +0330

    active cards styles

--- projects.js
+++ projects.js
@@ -1,6 +1,7 @@
 const menuItems = document.querySelectorAll('.menu-items li');
 const menuIcon = document.querySelector('.menu');
 const menu = document.querySelector('menu');
+const projectCards = document.querySelectorAll('.project-card');
 
 
 menuItems.forEach((item) => {
@@ -10,12 +11,27 @@ menuItems.forEach((item) => {
     })
 })
 
-if (!menuItems) { console.log('menuItems not found'); }
-
 menuIcon.addEventListener('click', () => {
     if (menu) {
         menu.style.display = menu.style.display === "hidden" ? "block" : "hidden";
     }
+})
+
+projectCards.forEach((projectCard, index) => {
+    if(localStorage.getItem(`card-${index}`) === "active" ){
+        projectCards.forEach((card) => {card.classList.remove('active-card');});
+        projectCard.classList.add('active-card');
+    }
+
+    projectCard.addEventListener('click', () => {
+        projectCard.classList.toggle('active-card');
+
+        if(projectCard.classList.contains('active-card')){
+            localStorage.setItem(`card-${index}`, 'active');
+        } else {
+            localStorage.removeItem(`card-${index}`);
+        }
+    })
 })
 
 

76a2cd14cc6e20f18e5339162b50ccbcff4a1366
eyeedev <eyeedev@gamil.com>
2025-02-09 17:20:50 +0330

    menu click handled

--- projects.js
+++ projects.js
@@ -2,6 +2,13 @@ const menuItems = document.querySelectorAll('.menu-items li');
 const menuIcon = document.querySelector('.menu');
 const menu = document.querySelector('menu');
 const projectCards = document.querySelectorAll('.project-card');
+const cardContainer = document.querySelector('.projects-cards');
+const cards = document.querySelectorAll('.project-card');
+let all = document.getElementById('all');
+let games = document.getElementById('games');
+let canvas = document.getElementById('canvas');
+let svg = document.getElementById('svg');
+let cssComponents = document.getElementById('css');
 
 
 menuItems.forEach((item) => {
@@ -34,4 +41,32 @@ projectCards.forEach((projectCard, index) => {
     })
 })
 
+
+function showCards(type) {
+   for(let i = 0; i < cards.length; i++){
+    let card = cards[i]
+    card.style.display = '';
+   }
+
+    cards.forEach(card => {
+        let cardType = card.getAttribute("data-type");
+        if (cardType !== type) {
+            card.style.display = "none";
+        }
+    });
+}
+
+canvas.addEventListener('click', () => showCards('canvas'));
+games.addEventListener('click', ()=>showCards('game'));
+svg.addEventListener('click', ()=>showCards('svg'));
+cssComponents.addEventListener('click', ()=>showCards('css'));
+
+
+
+all.addEventListener('click', ()=>{
+    for(let i = 0; i < cards.length; i++){
+        let card = cards[i]
+        card.style.display = '';
+       }
+});
 
--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-let all = document.getElementById('all');
-let games = document.getElementById('games');
-let canvasPart = document.getElementById('canvas');
-let svg = document.getElementById('svg');
-let cssPlaying = document.getElementById('css');
-let buttons = document.getElementById('buttons');
 let git = document.querySelector('.git-simulator');
 let currentIndex = 0;
-const cards = document.querySelector('.card');
-const cardContainer = document.querySelector('.cards');
 const indicators = document.querySelectorAll('.svg-item');
 const sections = document.querySelectorAll('section');
 const navLinks = document.querySelectorAll('nav ul li a');
@@ -56,32 +48,6 @@ navLinks.forEach(link => {
 
 
 
-// function showCards(type) {
-//    for(let i = 0; i < cards.length; i++){
-//     let card = cards[i]
-//     card.style.display = '';
-//    }
-//
-//     cards.forEach(card => {
-//         let cardType = card.getAttribute("data-type");
-//         if (cardType !== type) {
-//             card.style.display = "none";
-//         }
-//     });
-// }
 
-// canvasPart.addEventListener('click', () => showCards('canvas'));
-// games.addEventListener('click', ()=>showCards('game'));
-// svg.addEventListener('click', ()=>showCards('svg'));
-// cssPlaying.addEventListener('click', ()=>showCards('css'));
-// buttons.addEventListener('click', ()=>showCards('buttons'));
-
-
-// all.addEventListener('click', ()=>{
-//     for(let i = 0; i < cards.length; i++){
-//         let card = cards[i]
-//         card.style.display = '';
-//        }
-// });
 
 
